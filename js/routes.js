3c59abd7f9ca6fbdff1ce3dad17d6e033c0d8ad1
Chupee <julie.pronzac@gmail.com>
2014-10-18 21:13:50 +0200

    Debug photos

--- js/routes.js
+++ js/routes.js
@@ -14,7 +14,7 @@ app_router.on('route:index', function(actions) {
 
 	/* GOOGLE APIS ************************/
 	var map = null;
-	var focus = { lat: -34.397, lng: 150.644};
+	var focus = { lat: -30.397, lng: 150.644};
 	var mapOptions = {
 	  center: focus,
 	  zoom: 4
@@ -37,40 +37,44 @@ app_router.on('route:index', function(actions) {
 
 	var roadtrip, location, photo, marker = null;
 
+	// roadtrip
 	query = new Parse.Query(Roadtrip);
 	query.find().then(function(roadtrips){
 		roadtrip = roadtrips[0];
 		$("h1#roadtripName").html(roadtrip.get("title"));
+		// locations
 		query = new Parse.Query(Location);
 		query.equalTo("roadtrip", roadtrip);
 		return query.find();
 	}).then(function(locations){
 		$.each(locations, function(index, location) {
 			console.log(location);
-		    marker = drawMarker(map, { lat: location.get("coordinates").latitude, lng:  location.get("coordinates").longitude } );
+		    var marker = drawMarker(map, { lat: location.get("coordinates").latitude, lng:  location.get("coordinates").longitude } );
+			console.log("marker before photo");
+			console.log(marker);
 			query = new Parse.Query(Photo);
 			query.equalTo("location", location);
-			query.first({
-				  success: function(photo) {
-				  	console.log("nianaaniania");
-				  	console.log(photo);
-				  	if (photo != undefined) {
-				  	photoUrl = photo.get("file").url();
-					  	console.log(photo);
-					  	console.log(photoUrl);
+
+			query.find().then(function(photos) {
+				for (var i = 0; i < photos.length; i++) { 
+				  	if (photos[i] != undefined) {
+				  		console.log("PHOTO NOT NULL");
+				  		console.log(marker);
+  						photoUrl = photos[i].get("file").url();
 					      var contentString = '<img src="'+photoUrl+'" width="300"/>';
 						  var infowindow = new google.maps.InfoWindow({
 						      content: contentString
 						  });
+						  
 						  google.maps.event.addListener(marker, 'click', function() {
 					    	  infowindow.open(map,marker);
 						  });
 					}
-				  },
-				  error: function(error) {
-				    alert("Error: " + error.code + " " + error.message);
-				  }
-			});
+				}
+			})
+
+
+
 		}); 
 	}, function(error) {
 		console.log("Error: " + error.code + " " + error.message);
