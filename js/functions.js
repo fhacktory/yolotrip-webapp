1fa76502b0ad0d77a960327b513f67f8d14123c4
Chupee <julie.pronzac@gmail.com>
2014-10-18 18:31:49 +0200

    Afficher les points du roadtrip sur la carte

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,43 @@
 /* DRAWING ON THE MAP *****************/
-function drawMarker(map, position, backpacker) {
-	console.log(backpacker.attributes.username);
+function drawMarker(map, position) {
 	var marker = new google.maps.Marker({
 	    position: position,
 	    map: map,
-	    title: backpacker.attributes.username
 	   });
 	marker.setMap(map);
+}
+
+function setRelation() {
+
+	var User = Parse.Object.extend("User");
+	var Roadtrip = Parse.Object.extend("Roadtrip");
+	var Location = Parse.Object.extend("Location");
+	var Photo = Parse.Object.extend("Photo");
+
+	query = new Parse.Query(User);
+	query.find({
+	  success: function(results) {
+	  	console.log(results);
+	    query2 = new Parse.Query(Roadtrip);
+
+	  	for (var i = 0; i < results.length; i++) { 
+	      query2.first({
+			  success: function(result2) {
+			  	console.log(result2);
+			  	query2 = new Parse.Query(Photo);
+				result2.relation("user").add(results[0]);
+				result2.save();
+			  },
+			  error: function(error) {
+			    console.log("Error: " + error.code + " " + error.message);
+			  }
+			});
+	    }
+	  	
+	  },
+	  error: function(error) {
+	    console.log("Error: " + error.code + " " + error.message);
+	  }
+	});
+
 }
--- js/routes.js
+++ js/routes.js
@@ -12,49 +12,48 @@ var app_router = new AppRouter;
 app_router.on('route:index', function(actions) {
     console.log('index');
 
+	/* GOOGLE APIS ************************/
+	var map = null;
+	var focus = { lat: -34.397, lng: 150.644};
+	var mapOptions = {
+	  center: focus,
+	  zoom: 8
+	};
+	function initialize() {
+	        map = new google.maps.Map(document.getElementById('map-canvas'),
+	            mapOptions);
+	      }
+	google.maps.event.addDomListener(window, 'load', initialize);
 
-/* GOOGLE APIS ************************/
-var map = null;
-var focus = { lat: -34.397, lng: 150.644};
-var mapOptions = {
-  center: focus,
-  zoom: 8
-};
-function initialize() {
-        map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-google.maps.event.addDomListener(window, 'load', initialize);
+	/* PARSE ******************************/
+	setRelation();
 
+	var User = Parse.Object.extend("User");
+	var Roadtrip = Parse.Object.extend("Roadtrip");
+	var Location = Parse.Object.extend("Location");
+	var Photo = Parse.Object.extend("Photo");
 
+	var query = null;
 
-    /* PARSE ******************************/
-var User = Parse.Object.extend("User");
-var Roadtrip = Parse.Object.extend("Roadtrip");
-var Location = Parse.Object.extend("Location");
+	var roadtrip, location, photo = null;
 
-var query = null;
+	query = new Parse.Query(Roadtrip);
+	query.find().then(function(roadtrips){
+		roadtrip = roadtrips[0];
+		console.log(roadtrip);
+		query = new Parse.Query(Location);
+		query.equalTo("roadtrip", roadtrip);
+		return query.find();
+	}).then(function(locations){
+		$.each(locations, function(index, location) {
+			console.log(location);
+			console.log(location.get("coordinates").latitude);
+		    drawMarker(map, { lat: location.get("coordinates").latitude, lng:  location.get("coordinates").longitude } );
+		}); 	
+	}, function(error) {
+		console.log("Error: " + error.code + " " + error.message);
+	});
 
-query = new Parse.Query(User);
-query.first({
-  success: function(result) {
-  	drawMarker(map, focus, result);
-  },
-  error: function(error) {
-    console.log("Error: " + error.code + " " + error.message);
-  }
-});
-
-query = new Parse.Query(Roadtrip);
-var location = new Parse.Object("Location");
-query.first({
-  success: function(result) {
-	location.put("roadtrip", result);
-  },
-  error: function(error) {
-    console.log("Error: " + error.code + " " + error.message);
-  }
-});
 
 });
 
