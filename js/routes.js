a7c5ee27cb4529a4f64b34ba9ecab20ec04ad285
Chupee <julie.pronzac@gmail.com>
2014-10-19 01:50:59 +0200

    Afficher les messages en dessous des photos s'il y en a

--- js/routes.js
+++ js/routes.js
@@ -62,17 +62,30 @@ app_router.on('route:roadtrip', function(userslug, roadtripslug) {
 			query = new Parse.Query(Location);
 			query.equalTo("roadtrip", roadtrip);
 			query.include('photosArray');
+			query.include('messages');
 			query.find({
 				success: function(savedLocations) {
 			    console.log('location length'+savedLocations.length);
 			    for(var i=0; i<savedLocations.length; i++) {
 			    	location = savedLocations[i];
+			    	// photos
 			    	var photos = location.get('photosArray');
+			    	// messages
+			    	var messages = location.get('messages');
 			    	var marker = drawMarker(map, { lat: location.get("coordinates").latitude, lng:  location.get("coordinates").longitude }, photos.length );
 			    	var contentString = "";
-			    	for (var j=0; j<photos.length; j++) {
-			    		contentString += '<img src="'+photos[j].get('file').url()+'" width="300"/><br />';
-			    	}
+			    	if(photos != undefined) {
+				    	for (var j=0; j<photos.length; j++) {
+				    		contentString += '<img src="'+photos[j].get('file').url()+'" width="300"/><br />';
+				    	}
+				    }
+			    	if(messages != undefined) {
+				    	for (var k=0; k<messages.length; k++) {
+				    		console.log(messages[k]);
+				    		contentString += '<hr /><p>'+messages[k].get('Message')+'</p>';
+				    	}
+				    }
+			    	console.log(contentString);
 			    	  marker.contentString = contentString;
 					  google.maps.event.addListener(marker, 'click', function() {
 					  	infowindow.close();
@@ -107,16 +120,17 @@ app_router.on('route:test', function(id) {
 // Start Backbone history a necessary step for bookmarkable URL's
 Backbone.history.start();
 
-/*var query = new Parse.Query(Parse.Object.extend("Location"));
-query.include('photosArray');
+/*
+var query = new Parse.Query(Parse.Object.extend("Location"));
+query.include('messages');
 query.find({
 	success: function(savedLocations) {
     console.log('location length'+savedLocations.length);
     for(var i=0; i<savedLocations.length; i++) {
-    	var photos = savedLocations[i].get('photosArray');
-    	for (var j=0; j<photos.length; j++) {
-    		console.log(photos[j].get('fake'));
-    	}
+    	var photos = savedLocations[i].get('messages');
+    	if (photos)
+        	console.log(photos[0].get('Message'));
     }
   }
-});*/
+});
+*/
